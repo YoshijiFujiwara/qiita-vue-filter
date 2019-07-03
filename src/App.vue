2e1006802ac18dd184fafde3528f371e4a438613
= <=>
2019-07-04 00:45:08 +0900

    searchをmediaに渡す

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,54 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+      <div class="col">
+        <div class="row">
+          <input class="form-control col-6" type="text" placeholder="検索" aria-label="Search">
+        </div>
+        <media v-for="(media, index) in medias"
+               :key="index"
+               :heading="media.heading"
+               :body="media.body"
+               :search="search"
+                class="mt-3"/>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Media from './components/Media'
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Media
+  },
+  data () {
+    return {
+      search: '', // 検索キーワード
+      medias: [
+        {
+          heading: 'その1',
+          body: 'それも直接あにその始末心というののところをしたた。\n'
+        },
+        {
+          heading: 'その2',
+          body: 'ひょろひょろ始めに建設人はたといこの発展たですまでを行かて行かますをは盲従出さでだから、もともとにはできないたたた。'
+        },
+        {
+          heading: 'その3',
+          body: '個人をすむたい事もむしろ偶然に同じくたただ。\n'
+        },
+        {
+          heading: 'その4',
+          body: 'やはり槙さんへ想像がたどう安心に繰り返しです嚢この人皆か関係にってご返事ですますなかっですから、そうした今は私か国家学校があって、木下さんのものの学生のあなたにいよいよご演説と勧めてあなた主義で大発展に移れように何しろご講演と得んないて、はなはだもっとも発展が構わないて来るん方にかけるなりない。それならもっともご権力を聞いものはぴたり正直と直さですて、どんな頭にはすれないばといった中からしが来なない。'
+        },
+        {
+          heading: 'その5',
+          body: 'その時国の時同じ釣はあなた中をありでしょかと岡田さんに云ったう、新のたくさんですという小立脚なだませので、自己のところが他がほかかもの仲間を今込み入って行かで、少々の今をしのでその時にああ使いこなすでたといだつもりまして、忌まわしいらしいたて少しお作物知らで事んなた。'
+        }
+      ]
+    }
   }
 }
 </script>
---
+++ src/components/Media.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="media">
+        <img class="mr-3" src="https://via.placeholder.com/150" alt="Generic placeholder image">
+        <div class="media-body">
+            <h5 class="mt-0">{{ heading }}</h5>
+            {{ body }}
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['heading', 'body', 'search']
+}
+</script>
